d6c3223a2732aa7d1eb723c814f501e17ebe76cd
SomuSysAdmin <somu.sysadm@gmail.com>
2018-02-28 01:08:49 +0530

    Completed 1.2, started 1.3

---
+++ RHCE/dependency.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Installing dependencies for Fedora... [OR RPM based OS Package Manager]"
+dnf -y install  texlive*minted*	livetex*framed*	texlive*multirow*	python3	python3*pygments*
